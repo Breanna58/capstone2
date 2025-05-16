97bed0ea7e97f17d62c0c8f0027c7dc6ba8ebbd2
Breanna58 <reyesbreanna58@gmail.com>
2025-05-15 21:12:18 -0700

    added notes and lofin front end

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault()
+        try {
+            const res = await fetch('/api/auth/login,' {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json '}, 
+            body: JSON.stringify({ email, password }), 
+         })
+
+         const data = await res.json()
+            if (res.ok) {
+                localStorage.setItem('token', data.token)
+                navigate("/notes")
+            } else {
+                alert(data.message || 'Login Failed')
+
+            }
+        }  catch (err) {
+            console.log(err)
+
+        }
+    }
+
+    return (
+
+
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" /> 
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+        <button type="submit"> Login</button>
+
+        </form>
+    )
+
+}
+
---
+++ client/src/components/Notes.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'; 
+
+function Notes()  {
+
+    const [notes, setNotes] = useState ([]); 
+    const [newNote, setNewNote] = useState(''); 
+
+
+
+}
+
+//fetches notes from backend 
+useEffect(() => {
+    fetch('/api/notes')
+    .then(res => res.json())
+    .then(data => setNotes(data))
+    .catch(err => console.error(err)); 
+
+}, []); 
+
+//add a new note 
+
+
+
+const handleSubmit = async (e) => {
+
+    e.preventDefault(); 
+    const res = await fetch('/api/notes', {
+
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify({ text: newNote }), 
+
+
+
+    }); 
+
+    const createNote = await res.json(); 
+    setNotes([createdNote, ...notes]); 
+    setNewNote(''); 
+
+    return (
+
+        <div>
+
+            <h1>My notes</h1>
+            <form onSubmit={handleSubmit}> 
+            <input
+            value={newNote}
+            onChange={(e)=> setNewNote(e.target.value)}
+            placeholder='write  new note'
+             />
+             <button type="submit">Add Note </button>
+             </form>
+             <ul>
+
+                {notes.map(note => (
+
+                    <li key={note.id}>{note.txt}</li>
+                ))}
+             </ul>
+
+        </div>
+
+
+
+
+    ); 
+
+
+
+}; 
+
+export default notes; 
